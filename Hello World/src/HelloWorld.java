0a6ad86302f1e03e44396b47ca2a08a28208b80b
mhs <mhs@MHS-LT9-26>
2019-08-21 12:19:33 -0700

    complete hello world project

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println ("Hello, world!");
+		System.out.println ("Maryam Hassan, August 19,2019, To make Java programs");
+		System.out.println ("AP Computer Science A");
+		System.out.println ("main mathod");
+		System.out.println ("println");
+	}
+}
