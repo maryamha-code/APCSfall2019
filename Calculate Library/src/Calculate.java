7b176b0c089a614cec1cce8f53154da7b1e92658
mhs <mhs@MHS-LT9-26>
2019-09-04 12:09:35 -0700

    Access Calculate

---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,11 @@
+*/*Maryam Hassan
+  * August 29, 2019
+  * Period 4
+  *Group of methods that perform math calculations
+*/
+public class Calculate {
+	public static int square(int number) {
+		return number*number;
+	}
+
+}
---
+++ Calculate Library/src/DoMath.java
@@ -0,0 +1,14 @@
+* /* Maryam Hassan
+  * August 29, 2019
+  * Period 4
+  * Call math methods from Calculate Library
+  * Client code or runner code
+  */
+public class DoMath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
